43a94c6e7aa888ecae289946d54f086b094fd191
Shamim Patel <shamim.patel@gmail.com>
2013-03-06 16:37:51 +0000

    Started SpectrumData loader

---
+++ SpectrumData.h
@@ -0,0 +1,16 @@
+//
+//  SpectrumData.h
+//  TestCode
+//
+//  Created by Shamim Patel on 06/03/2013.
+//  Copyright (c) 2013 Shamim Patel. All rights reserved.
+//
+
+#ifndef _SPECTRUMDATA_H_
+#define _SPECTRUMDATA_H_
+
+
+
+
+
+#endif
